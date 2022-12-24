b8f80c65b1e4b69868aa8751bad1ee384a0f6077
c2k-secundus <chinkheng_98@hotmail.com>
2022-12-24 16:35:58 +0800

    Completed navigation layover components

--- src/App.js
+++ src/App.js
@@ -2,12 +2,16 @@ import NavBar from "./components/NavBar";
 
 const App = () => {
   return (
-    <div className="flex flex-col">
+    <div className="flex w-screen max-w-full flex-col">
       <NavBar />
-      <h1 className="">Hey</h1>
-      <div className="h-screen w-screen">
-        <div className="flex-grow bg-red-200 "> Guhh</div>
-        <div className="flex-grow bg-red-200"> Guhh</div>
+
+      <div className="relative h-screen w-full justify-center text-center">
+        <div className="absolute top-[24%] w-full bg-white bg-opacity-0 text-center font-anton text-[9.375rem] font-normal text-custom-theme-purple">
+          HOME TO ALL.
+        </div>
+      </div>
+      <div className="h-screen w-full">
+        <h1 className="h-screen bg-green-600 text-5xl">green</h1>
       </div>
     </div>
   );
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,13 +1,16 @@
 import NavItem from "./NavItem";
 
+import { SiTwitter, SiDiscord, SiSpotify } from "react-icons/si";
+import { RiInstagramFill } from "react-icons/ri";
+import { MdEmail } from "react-icons/md";
 const NavBar = () => {
   return (
-    <div className="h-screen w-screen">
-      <div className="flex flex-grow justify-center bg-black h-[5.5%] ">
+    <div className="fixed z-10 h-screen w-screen">
+      <div className="flex h-[5.5%] flex-grow justify-center bg-black ">
         <div className="font-anton text-5xl text-white">LAZYNAIRE</div>
       </div>
 
-      <div className="flex flex-row flex-grow items-center space-x-16 justify-center bg-custom-theme-purple h-[5.5%]">
+      <div className="flex h-[5.5%] flex-grow flex-row items-center justify-center space-x-16 bg-custom-theme-purple">
         <NavItem text={"HOME"} textLink={"#"} />
         <NavItem text={"EXPLORE"} textLink={"#"} />
         <NavItem text={"CORE"} textLink={"#"} />
@@ -15,6 +18,39 @@ const NavBar = () => {
         <NavItem text={"TEAM"} textLink={"#"} />
         <NavItem text={"FAQ"} textLink={"#"} />
         <NavItem text={"MINT"} textLink={"#"} />
+      </div>
+
+      <div className="absolute bottom-5 w-full ">
+        <div className="flex flex-row items-center justify-center space-x-6">
+          <a
+            className="rounded-full bg-custom-theme-purple p-2"
+            href="https://twitter.com/"
+          >
+            <SiTwitter size={30} color={"white"} />
+          </a>
+          <a
+            className="rounded-full bg-custom-theme-purple p-2"
+            href="https://discord.com/"
+          >
+            <SiDiscord size={30} color={"white"} />
+          </a>
+
+          <a
+            className="rounded-full bg-custom-theme-purple p-2"
+            href="https://www.instagram.com/"
+          >
+            <RiInstagramFill size={30} color={"white"} />
+          </a>
+          <a className="rounded-full bg-custom-theme-purple p-2" href="#">
+            <MdEmail size={30} color={"white"} />
+          </a>
+          <a
+            className=" rounded-full bg-custom-theme-purple p-2"
+            href="https://open.spotify.com/"
+          >
+            <SiSpotify size={30} color={"white"} />
+          </a>
+        </div>
       </div>
     </div>
   );
