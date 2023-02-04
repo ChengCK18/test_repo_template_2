a0cad14bb5e1c7005bf6e3722e4c94702fb78570
ChengCK18 <chinkheng_98@hotmail.com>
2023-02-04 14:02:50 +0800

    Replace existing codebase for homepage

--- src/App.js
+++ src/App.js
@@ -1,16 +1,154 @@
+import { useState, useEffect } from "react";
 import NavBar from "./components/NavBar";
-import AboutIntro from "./components/AboutIntro";
-const App = () => {
-  return (
-    <div className="flex w-screen max-w-full flex-col">
-      <NavBar />
 
-      <div className="relative h-screen w-full justify-center  text-center">
-        <div className="absolute top-[24%] w-full bg-white bg-opacity-0 text-center font-anton font-normal tracking-wider text-custom-theme-purple mobile:px-20 mobile:text-6xl laptop:text-[9.375rem]">
-          HOME TO ALL.
+const App = () => {
+  const [userDevice, setUserDevice] = useState("laptop");
+
+  useEffect(() => {
+    let screenWidth = window.innerWidth;
+
+    if (screenWidth < 640) {
+      setUserDevice("mobile");
+    } else if (screenWidth < 821) {
+      setUserDevice("tablet");
+    } else {
+      setUserDevice("laptop");
+    }
+    window.addEventListener("resize", handleResize);
+  }, []);
+
+  const handleResize = () => {
+    let screenWidth = window.innerWidth;
+    if (screenWidth < 640) {
+      setUserDevice("mobile");
+    } else if (screenWidth < 821) {
+      setUserDevice("tablet");
+    } else {
+      setUserDevice("laptop");
+    }
+  };
+
+  console.log(userDevice);
+
+  let imageLayout = "";
+  if (userDevice === "laptop") {
+    imageLayout = (
+      <>
+        <div className="absolute flex h-full w-screen justify-center">
+          <img
+            className="h-full w-screen"
+            src="/images/merged/Desktop_Back_01.png"
+            alt="Desktop_Back_01"
+          />
         </div>
+        <div className="absolute flex h-full w-screen items-end justify-center">
+          <div className={`h-[85%] shrink-0 `}>
+            <img
+              className="h-full "
+              src="/images/merged/Desktop_Middle_01_2.png"
+              alt="Desktop_Middle_01"
+            />
+          </div>
+        </div>
+
+        <div className="absolute z-40 flex h-full w-screen items-end justify-center">
+          <div className={`h-[78%] shrink-0 `}>
+            <img
+              className="h-full"
+              src="/images/merged/Desktop_Front_01.png"
+              alt="Desktop_Middle_01"
+            />
+          </div>
+        </div>
+      </>
+    );
+  } else if (userDevice === "mobile") {
+    imageLayout = (
+      <>
+        {/* <div className="absolute flex h-screen w-screen justify-center items-end ">
+                    <div className="shrink-0">
+                        <img className="h-[420px] w-[790px] " src="/images/merged/Mobile_Back_01.png" alt="car_pink" />
+                    </div>
+                </div>
+                <div className="absolute z-30 h-screen w-screen flex justify-center items-end">
+                    <div className="shrink-0">
+                        <img className="h-[450px] w-[750px]" src="/images/merged/Mobile_Front_01.png" alt="car_pink" />
+                    </div>
+                </div> */}
+
+        <div className="absolute inset-0 flex h-full w-screen items-end justify-center ">
+          <div className="h-[60%]">
+            <img
+              className="h-full object-cover"
+              src="/images/merged/Mobile_Back_01.png"
+              alt="car_pink"
+            />
+          </div>
+        </div>
+        <div className="absolute inset-0 z-30  flex h-full w-screen items-end justify-center">
+          <div className="h-[60%] ">
+            <img
+              className="h-full object-cover"
+              src="/images/merged/Mobile_Front_01.png"
+              alt="car_pink"
+            />
+          </div>
+        </div>
+      </>
+    );
+  } else if (userDevice === "tablet") {
+    imageLayout = (
+      <>
+        <div className="absolute inset-0 flex  h-full w-screen justify-center">
+          <img
+            className="h-full w-screen"
+            src="/images/merged/Desktop_Back_01.png"
+            alt="Desktop_Back_01"
+          />
+        </div>
+        <div className="absolute inset-0 flex h-full w-screen items-end justify-center">
+          <div className={`h-[78%] shrink-0`}>
+            <img
+              className="h-full "
+              src="/images/merged/Tablet_Middle_01.png"
+              alt="Desktop_Middle_01"
+            />
+          </div>
+        </div>
+
+        <div className="absolute inset-0 z-40  flex h-full w-screen items-end justify-center">
+          <div className={`h-[75%] shrink-0`}>
+            <img
+              className="h-full"
+              src="/images/merged/Tablet_Front_01.png"
+              alt="Desktop_Middle_01"
+            />
+          </div>
+        </div>
+      </>
+    );
+  }
+
+  return (
+    <div className="relative flex h-full w-screen flex-col overflow-hidden">
+      <NavBar userDevice={userDevice} />
+
+      <div className="absolute inset-0  h-full w-full justify-center  text-center">
+        {/* <div className={`absolute z-20  ${userDevice === 'mobile' ? 'bottom-[63vh]' : 'bottom-[68vh]'}  w-full bg-white bg-opacity-0 text-center font-anton font-normal tracking-wider text-custom-theme-purple leading-none  mobile:text-[58px] tablet:text-[15.2vh] laptop:text-[150px]`}>
+                        <p className={`${screenHeight < 500 ? '' : ''}`}>HOME TO {userDevice === 'mobile' ? <br /> : ''} ALL.</p>
+                    </div> */}
+        <div
+          className={`absolute z-20  ${
+            userDevice === "mobile" ? "bottom-[60vh]" : "bottom-[68vh]"
+          }  w-full bg-white bg-opacity-0 text-center font-anton font-normal leading-none tracking-wider text-custom-theme-purple mobile:text-[10.2vh] laptop:text-[13.8vh]`}
+        >
+          <p className={``}>
+            HOME TO {userDevice === "mobile" ? <br /> : ""} ALL.
+          </p>
+        </div>
+
+        {imageLayout}
       </div>
-      <AboutIntro />
     </div>
   );
 };
