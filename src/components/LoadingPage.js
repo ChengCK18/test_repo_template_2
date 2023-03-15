fa829571894abc3aef314e33bc1a24680b6b597b
Cheng Chin Kheng <chinkheng_98@hotmail.com>
2023-03-15 23:47:41 +0800

    test scroll

--- src/components/LoadingPage.js
+++ src/components/LoadingPage.js
@@ -14,9 +14,10 @@ const LoadingPage = ({ loading }) => {
         <div
             className={`${
                 loading ? "" : "animate-fadeOutAniLoading"
-            } ${loadingHidden} absolute top-0 z-50 h-full w-full bg-black bg-cover bg-center mobile:bg-about_bg_img_mobile tablet:bg-about_bg_img_tablet laptop:bg-about_bg_img_laptop`}
+            } ${loadingHidden} absolute top-0 z-50 h-full w-full overflow-hidden bg-black`}
         >
-            <div className="relative flex h-full w-full items-center justify-center">
+            <div className="absolute top-0  h-[200vh] w-full bg-black"></div>
+            <div className="relative flex h-full w-full items-center justify-center bg-cover bg-center mobile:bg-about_bg_img_mobile tablet:bg-about_bg_img_tablet laptop:bg-about_bg_img_laptop">
                 <div>
                     <div
                         className="animate-spin rounded-full  border-[12px] border-double border-gray-300 border-t-transparent mobile:h-14 mobile:w-14 tablet:h-24
