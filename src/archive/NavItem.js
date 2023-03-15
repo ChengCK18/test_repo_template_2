649c267de4407288ddc28f06ddb558f09f46a15e
Cheng Chin Kheng <chinkheng_98@hotmail.com>
2023-03-15 21:05:46 +0800

    try music

--- src/archive/NavItem.js
+++
@@ -1,9 +0,0 @@
-const NavItem = ({ text, textLink }) => {
-  return (
-    <div className="font-neueHaas text-xl font-semibold  text-white  ">
-      <a href={textLink}>{text}</a>
-    </div>
-  );
-};
-
-export default NavItem;
