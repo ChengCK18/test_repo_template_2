e1fbef637783f56f11feada16f97ef14956f339c
Cheng Chin Kheng <chinkheng_98@hotmail.com>
2023-06-18 13:36:41 +0800

    Added home and share tweet button on successful mint

--- src/components/MintPage/TransactionConfirm.js
+++ src/components/MintPage/TransactionConfirm.js
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
-
+import { Link } from "react-router-dom";
+import { TwitterIcon, TwitterShareButton } from "react-share";
 const TransactionConfirm = ({ confirmingTransac, setConfirmingTransac }) => {
     const [heading, setHeading] = useState();
     const [subtext, setSubtext] = useState();
@@ -32,9 +33,29 @@ const TransactionConfirm = ({ confirmingTransac, setConfirmingTransac }) => {
                 </>
             );
             setSubtext(
-                <>
-                    <>Welcome to Lazynaire club</>
-                </>
+                <div className="flex w-1/2 flex-col items-center justify-center space-y-16 ">
+                    <div>Welcome to Lazynaire club</div>
+                    <div className="flex w-full flex-row space-x-16">
+                        <Link
+                            to="/"
+                            onClick={() => {
+                                setConfirmingTransac(0);
+                            }}
+                            className="flex-1 rounded-full border-2 border-custom-theme-purple bg-white p-3 text-custom-theme-purple"
+                        >
+                            Home
+                        </Link>
+                        <TwitterShareButton
+                            url={"https://www.lazynaire.com/"}
+                            title={"I just minted from Lazynaire project!"}
+                            className="flex-1"
+                        >
+                            <div className="rounded-full border-2 bg-custom-theme-purple p-3 text-white">
+                                Share Tweet
+                            </div>
+                        </TwitterShareButton>
+                    </div>
+                </div>
             );
         } else if (confirmingTransac === 4) {
             setHeading(
@@ -62,10 +83,10 @@ const TransactionConfirm = ({ confirmingTransac, setConfirmingTransac }) => {
     return (
         <div className="absolute top-0  flex  h-full w-full items-center justify-center">
             <div className="flex h-3/5 w-3/5 flex-col items-center justify-center rounded-2xl bg-white">
-                <div className="mt-10 text-center font-neueHaas text-6xl font-semibold leading-[60px] text-custom-theme-purple">
+                <div className="mt-10 w-full text-center font-neueHaas text-6xl font-semibold leading-[60px] text-custom-theme-purple">
                     {heading}
                 </div>
-                <div className="mt-10 text-center font-neueHaas text-2xl font-semibold leading-[25px] text-custom-theme-purple">
+                <div className="mt-10 flex w-full items-center justify-center text-center font-neueHaas text-2xl font-semibold leading-[25px] text-custom-theme-purple">
                     {subtext}
                 </div>
             </div>
